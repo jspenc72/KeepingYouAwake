adde7bef517b33b6fe31f712a82d59718478b252
Marcel Dierkes <marcel.dierkes@gmail.com>
2016-10-30 21:37:34 +0100

    Documentation update

--- KYACaffeinate/KYACaffeinateTask.h
+++ KYACaffeinate/KYACaffeinateTask.h
@@ -10,6 +10,9 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ An exception that will be thrown when the `caffeinate` utility is not available.
+ */
 extern NSExceptionName const KYACaffeinateUnavailableException;
 
 typedef void(^KYACaffeinateTaskCompletionHandler)(BOOL cancelled);
@@ -17,19 +20,60 @@ typedef void(^KYACaffeinateTaskCompletionHandler)(BOOL cancelled);
 @class KYACaffeinateTaskConfiguration;
 @protocol KYACaffeinateTaskDelegate;
 
+/**
+ A task-based interface around the `caffeinate` utility.
+ */
 @interface KYACaffeinateTask : NSObject
+
+/**
+ The configuration object for this task.
+ */
 @property (copy, nonatomic, readonly) KYACaffeinateTaskConfiguration *configuration;
+
+/**
+ The task delegate to receive state changes.
+ */
 @property (weak, nonatomic) id<KYACaffeinateTaskDelegate> delegate;
 
+/**
+ A completion handler.
+ */
 @property (copy, nonatomic, readonly) KYACaffeinateTaskCompletionHandler completionHandler;
+
+/**
+ Returns the completion date for the currently scheduled timer.
+ 
+ If the timer was scheduled for 0 seconds (Infinity), the fireDate is nil.
+ */
 @property (copy, nonatomic, readonly, nullable) NSDate *fireDate;
+
+
+/**
+ Return YES if the task is spawned and running.
+ */
 @property (nonatomic, readonly, getter=isRunning) BOOL running;
 
 - (instancetype)init NS_UNAVAILABLE;
+
+/**
+ The designated initializer for the caffeinate task.
+
+ @param configuration A configuration object.
+ @return A new instance.
+ */
 - (instancetype)initWithConfiguration:(KYACaffeinateTaskConfiguration *)configuration NS_DESIGNATED_INITIALIZER;
 
+/**
+ Spawns the configured task, calls the completion handler and notifies its delegate.
+
+ @param completionHandler A completion handler.
+ @return YES if the task was spawned successfully. If this returns NO, completionHandler won't be called.
+ */
 - (BOOL)spawnWithCompletionHandler:(KYACaffeinateTaskCompletionHandler)completionHandler;
 
+/**
+ Cancels the spawned task.
+ */
 - (void)cancel;
 
 @end
--- KYACaffeinate/KYACaffeinateTaskConfiguration.h
+++ KYACaffeinate/KYACaffeinateTaskConfiguration.h
@@ -10,23 +10,101 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ An indefinte timeout (usually representing a value of 0 seconds).
+ */
 extern NSTimeInterval const KYACaffeinateTaskConfigurationTimeoutIndefinite;
+
+/**
+ A default timeout (usually 5 seconds).
+ */
 extern NSTimeInterval const KYACaffeinateTaskConfigurationTimeoutDefault;
 
+
+/**
+ A caffeinate task configuration describes the behaviors that a
+ spawned caffeinate task should provide.
+ */
 @interface KYACaffeinateTaskConfiguration : NSObject
-@property (nonatomic, getter=isPreventingDisplaySleep) BOOL preventDisplaySleep;  // -d
-@property (nonatomic, getter=isPreventingSystemIdleSleep) BOOL preventSystemIdleSleep;  // -i
-@property (nonatomic, getter=isPreventingDisplayIdleSleep) BOOL preventDisplayIdleSleep;  // -m
-@property (nonatomic, getter=isPreventingSystemSleepOnACPower) BOOL preventSystemSleepOnACPower;  // -s
-@property (nonatomic, getter=isUserActive) BOOL userActive;  // -u
 
-@property (nonatomic) NSTimeInterval timeout; // defaults to 5secs  // -t
-@property (nonatomic, nullable) NSNumber *pinnedProcessID;  // defaults to nil  // -w
+/**
+ Create an assertion to prevent the display from sleeping.
+ 
+ (The -d command line flag)
+ */
+@property (nonatomic, getter=isPreventingDisplaySleep) BOOL preventDisplaySleep;
 
-+ (instancetype)defaultConfiguration;  // indefinite timeout
+/**
+ Create an assertion to prevent the system from idle sleeping.
+ 
+ (The -i command line flag)
+ */
+@property (nonatomic, getter=isPreventingSystemIdleSleep) BOOL preventSystemIdleSleep;
+
+/**
+ Create an assertion to prevent the disk from idle sleeping.
+ 
+ (The -m command line flag)
+ */
+@property (nonatomic, getter=isPreventingDisplayIdleSleep) BOOL preventDisplayIdleSleep;
+
+/**
+ Create an assertion to prevent the system from sleeping. This assertion is valid only
+ when system is running on AC power.
+ 
+ (The -s command line flag)
+ */
+@property (nonatomic, getter=isPreventingSystemSleepOnACPower) BOOL preventSystemSleepOnACPower;
+
+/**
+ Create an assertion to declare that user is active. If the display is off, this option
+ turns the display on and prevents the display from going into idle sleep.
+ 
+ (The -u command line flag)
+ */
+@property (nonatomic, getter=isUserActive) BOOL userActive;
+
+/**
+ Specifies the timeout value in seconds for which this assertion has to be valid.
+ 
+ Defaults to 5 seconds.
+ 
+ (The -t command line flag)
+ */
+@property (nonatomic) NSTimeInterval timeout;
+
+
+/**
+ Waits for the process with the specified pid to exit. Once the the process exits, the
+ assertion is also released.
+ 
+ (The -w command line flag)
+ */
+@property (nonatomic, nullable) NSNumber *pinnedProcessID;
+
+/**
+ A default configuration with indefinte timeout.
+ */
++ (instancetype)defaultConfiguration;
+
+/**
+ A default configuration with timeout.
+
+ @param timeout A timeout.
+ */
 + (instancetype)defaultConfigurationWithTimeout:(NSTimeInterval)timeout;
+
+/**
+ A default configuration with timeout and process ID to pin.
+
+ @param timeout A timeout.
+ @param pinnedProcessID A process ID to which the task is pinned.
+ */
 + (instancetype)defaultConfigurationWithTimeout:(NSTimeInterval)timeout pinnedProcessID:(nullable NSNumber *)pinnedProcessID;
 
+/**
+ The designated initializer.
+ */
 - (instancetype)init NS_DESIGNATED_INITIALIZER;
 
 @end
